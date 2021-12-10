a5c3a5a9710ba0f5f49cc5259f0ba059f6e46cfa
gjy <1265452170@qq.com>
2021-12-10 19:50:05 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,6 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("1111");
+        System.out.println("1222");
     }
 }
